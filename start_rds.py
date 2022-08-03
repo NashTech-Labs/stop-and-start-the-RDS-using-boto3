6c509d4be5d949861d858727fa68f3a543f49c45
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-03 16:58:45 +0530

    Update in code

--- start_rds.py
+++ start_rds.py
@@ -1,8 +1,17 @@
 # import the boto3 which will use to interact  with the aws
 import boto3
+# We will take the all imput from the end user.
 
-client = boto3.client('rds')
-response = client.start_db_instance(
-     DBInstanceIdentifier=input("Enter the database name")
+# You need to enter rds as input
+AWS_service= input("Enter the service name\n")
+
+database_name=input("Enter the database name\n")
+
+client = boto3.client(AWS_service)
+
+start_rds = client.start_db_instance(
+     DBInstanceIdentifier= database_name
 )
-print(response)
+print(start_rds)
+
+print("Your RDS has been started Successfully ")
--- stop_rds.py
+++ stop_rds.py
@@ -1,10 +1,21 @@
 # import the boto3 which will use to interact  with the aws
 import boto3
+# We will take the all imput from the end user.
 
-client = boto3.client('rds')
+# You need to enter rds as input
+AWS_service= input("Enter the service name\n")
 
-response = client.stop_db_instance(
-    DBInstanceIdentifier=input("Enter the database name"),
-    DBSnapshotIdentifier=input("Enter the snapshot name")
+database_name=input("Enter the database name\n")
+
+# you need to enter the snapshot name for your RDS instance
+snapshot_name=input("Enter the snapshot name\n")
+
+client = boto3.client(AWS_service)
+
+stop_rds = client.stop_db_instance (
+    DBInstanceIdentifier= database_name,
+    DBSnapshotIdentifier=snapshot_name
 )
-print(response)
+print(stop_rds)
+
+print("Your RDS has been stoped Successfully ")
